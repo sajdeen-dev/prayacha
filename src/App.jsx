cc88c0179de3169464203fa9a5c20bb03e8ca98a
sajdeen-dev <sayyedsajdeen.dev@gmail.com>
2026-02-05 23:20:41 +0530

    in App component: Remove unnecessary whitespace for cleaner code.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 import Products from './pages/Products'
 import About from './pages/About'
 import Contact from './pages/Contact'
-
+ 
 function App() {
   return (
     <BrowserRouter>
